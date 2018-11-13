92d6efc30ccb6daba7ada2d3603e0550b2fbc7bf
h8p1b <stevensqiu@gmail.com>
2018-11-12 18:25:08 -0800

    Update mspa_drop_tables.sql
    added drop table sql file

---
+++ mspa_drop_tables.sql
@@ -0,0 +1,23 @@
+drop table CarryPassengerShipment;
+
+drop table Passenger;
+
+drop table CrewShipment;
+
+drop table ShipmentLaunchpadSlot;
+
+drop table Officer;
+
+drop table Cargo;
+
+drop table Crew;
+
+drop table Reservation;
+
+drop table Rocket;
+
+drop table Astronaut;
+
+drop table Company;
+
+commit;
