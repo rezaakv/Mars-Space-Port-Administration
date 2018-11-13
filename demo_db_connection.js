f0a1f1f1d76d2b191d4be7845a663b5f72731c72
Qianqian <d9p1b@cs.ubc.ca>
2018-11-12 17:02:54 -0800

    add nodeJS connection test

---
+++ demo_db_connection.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: '138.68.57.55',
+  port: 3306,
+  user: "Qianqian",
+  password: "Cpsc304!",
+  database: "MarsSpacePortAdministration"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "create table test(a int, b int)";
+  con.query(sql, function (err, result) {
+  if (err) throw err;
+  console.log("1 table created");
+  });
+});
