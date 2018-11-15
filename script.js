17b6b62b2173521afd9c88a165932dfab2edf93f
QianqianF <tiffanyqqfeng@gmail.com>
2018-11-15 12:59:51 -0800

    updated login css

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/assets",express.static(__dirname + "/assets"));
+app.use("/images",express.static(__dirname + "/images"));
 
 app.get('/', function (req, res) {
   var err_msg = "";
