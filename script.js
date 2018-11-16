6636761186da068d386bb678a35d2ac5db6ce51b
h8p1b <stevensqiu@gmail.com>
2018-11-15 20:32:49 -0800

    Update officer.ejs, reformatting

--- script.js
+++ script.js
@@ -15,83 +15,78 @@ app.get('/', function (req, res) {
 
 
 app.post('/', function (req, res) {
-  // res.render('index');
-  var username = req.body.user;
-  var password = req.body.pass;
-  // console.log(username);
-  // console.log(password);
-  let con = mysql.createConnection({
-  	host: '138.68.57.55',
-  	port: 3306,
-  	user: username,
-  	password: password,
-  	database: "MarsSpacePortAdministration"
-  });
+	// res.render('index');
+  	var username = req.body.user;
+  	var password = req.body.pass;
+  	// console.log(username);
+  	// console.log(password);
+  	let con = mysql.createConnection({
+  		host: '138.68.57.55',
+  		port: 3306,
+  		user: username,
+  		password: password,
+		database: "MarsSpacePortAdministration"
+	});
 
-  con.connect(function(err) {
-    if (err) {
-	console.log(err);
-	var err_msg = "Permission denied. Please try again.";
-	res.render('index', {err_msg: err_msg});
-    } else {
-	var table = [];
-	//res.render('officer', {table: table})
-	console.log('officer rendered');
-	//console.log(con);
-  	//app.get('/#', function (req, res){
-	//	console.log('listening to officer')
+  	con.connect(function(err) {
+    	if (err) {
+			console.log(err);
+			var err_msg = "Permission denied. Please try again.";
+			res.render('index', {err_msg: err_msg});
+    	} else {
+		var table = [];
+		//res.render('officer', {table: table})
+		console.log('officer rendered');
+		//console.log(con);
+  		//app.get('/#', function (req, res){
+		//	console.log('listening to officer')
 		//console.log(con);
 		let sql = "select * from Astronaut";
-        	con.query(sql, function (err, result) {
-                	if (err) throw err;
+    	con.query(sql, function (err, result) {
+        	if (err) throw err;
 			//console.log(table);
 			res.render('officer', {table: result});
 		});
-	//});
-	//console.log(table)
-	//res.render('officer', {table: table});
+		//});
+		//console.log(table)
+		//res.render('officer', {table: table});
 
-	app.post('/astro', function (req, res){
-	//	console.log('got request');
-		var astroID = req.body.AstroID;
-		var name = req.body.Name;
-		if ((astroID == undefined && name == undefined) || (astroID == "" && name == "")) { // || (name=="" && isNaN(astroID))
-			sql = "select * from Astronaut";
-		} else {
-			sql = "select *  from Astronaut where ";
-			if (astroID != "") sql += "AstroID = " + astroID + " AND ";
-			if (name != "") sql += "Name = '" + name + "'" + " AND ";
-			sql = sql.slice(0, -5);
-		}
-		console.log(sql);
-		con.query(sql, function (err, result) {
-                        if (err) throw err;
-                        //console.log(table);
-                        res.render('officer', {table: result});
-                });
-	
+		app.post('/astro', function (req, res){
+			//	console.log('got request');
+			var astroID = req.body.AstroID;
+			var name = req.body.Name;
+			if ((astroID == undefined && name == undefined) || (astroID == "" && name == "")) { // || (name=="" && isNaN(astroID))
+				sql = "select * from Astronaut";
+			} else {
+				sql = "select *  from Astronaut where ";
+				if (astroID != "") sql += "AstroID = " + astroID + " AND ";
+				if (name != "") sql += "Name = '" + name + "'" + " AND ";
+				sql = sql.slice(0, -5);
+			}
+			console.log(sql);
+			con.query(sql, function (err, result) {
+                if (err) throw err;
+                //console.log(table);
+                res.render('officer', {table: result});
+			});
+		});
 
-	});
-
-	app.post('/deleteAstro', function (req, res){
-		console.log("deleteClicked")
-		var astroID = req.body.AstroID;
-		console.log(astroID);
+		app.post('/deleteAstro', function (req, res){
+			console.log("deleteClicked")
+			var astroID = req.body.AstroID;
+			console.log(astroID);
 		
-		sql = "delete from Astronaut where AstroID = "+astroID;
-		console.log(sql);
-                con.query(sql, function (err, result) {
-                        if (err) throw err;
-                        //console.log(table);
-			con.query("select * from Astronaut", function(err, result){
-				if (err) throw err;
-                        	res.render('officer', {table: result});
-			})
-                });
-		
-
-
-	});
+			sql = "delete from Astronaut where AstroID = "+astroID;
+			console.log(sql);
+            con.query(sql, function (err, result) {
+                if (err) throw err;
+                //console.log(table);
+				con.query("select * from Astronaut", function(err, result){
+					if (err) throw err;
+					res.render('officer', {table: result});
+				})
+			});
+		});
     }
   });
 })
